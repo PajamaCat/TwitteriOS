64f726fc3079da60105e041dc52eda085019bd0d
Jiafang Jiang <jiafang.jiang@airbnb.com>
2017-04-05 16:29:22 -0700

    init

---
+++ TwitteriOS/LoginViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LoginViewController.swift
+//  TwitteriOS
+//
+//  Created by jiafang_jiang on 4/5/17.
+//  Copyright © 2017 jiafang_jiang. All rights reserved.
+//
+
+import UIKit
+import BDBOAuth1Manager
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+  @IBAction func onLoginClicked(_ sender: UIButton) {
+  }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
