e0955c1b68d210bb4de0f33c3ed6a941817a5137
Jiafang Jiang <jiafang.jiang@airbnb.com>
2017-04-10 13:47:19 -0700

    add more stuff

--- TwitteriOS/DetailedTweetViewController.swift
+++ TwitteriOS/DetailedTweetViewController.swift
@@ -9,16 +9,29 @@
 import UIKit
 
 class DetailedTweetViewController: UIViewController {
+  
+  var tweet: Tweet!
 
+  @IBOutlet weak var content: UILabel!
+  @IBOutlet weak var timestamp: UILabel!
+  @IBOutlet weak var screenName: UILabel!
+  @IBOutlet weak var username: UILabel!
+  @IBOutlet weak var profileImage: UIImageView!
+  
   override func viewDidLoad() {
-      super.viewDidLoad()
+    super.viewDidLoad()
 
-      // Do any additional setup after loading the view.
+    // Do any additional setup after loading the view.
+    content.text = tweet.text
+    timestamp.text = tweet.timestamp
+    screenName.text = tweet.user?.screenName
+    username.text = tweet.user?.name
+    profileImage.setImageWith((tweet.user?.profileUrl)!)
   }
 
   override func didReceiveMemoryWarning() {
-      super.didReceiveMemoryWarning()
-      // Dispose of any resources that can be recreated.
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
   }
   
 
