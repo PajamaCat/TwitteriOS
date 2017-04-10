7fe3a13b73f76c799c2a427136e38314b45c5857
Jiafang Jiang <jiafang.jiang@airbnb.com>
2017-04-10 13:48:11 -0700

    Add action bar

---
+++ TwitteriOS/BottomActionBar.swift
@@ -0,0 +1,21 @@
+//
+//  BottomActionBar.swift
+//  TwitteriOS
+//
+//  Created by jiafang_jiang on 4/10/17.
+//  Copyright © 2017 jiafang_jiang. All rights reserved.
+//
+
+import UIKit
+
+class BottomActionBar: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
